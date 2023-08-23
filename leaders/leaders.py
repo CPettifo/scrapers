40655b251b925f86007d321b3e905a0abee33a5c
CPettifo <cpettifo@myune.edu.au>
2023-08-23 17:14:44 +0200

    print out head of state + gov for requested country

--- leaders/leaders.py
+++ leaders/leaders.py
@@ -16,14 +16,23 @@ table = soup.find('table',{'class':"wikitable"})
 df = pd.read_html(str(table))
 df=pd.DataFrame(df[0])
 
-row = str(df.loc[df.index[12]])
+row = str(df.loc[df.index[35]])
+
 
 split = row.split('\n')
 
-state_split = split[0].split(' ')
+state_split = split[0].split('  ')
 
 state = state_split[len(state_split) - 1]
 
-print(state)
+head_state_split = split[1].split('  ')
+
+head_state = head_state_split[len(head_state_split) - 1]
+
+gov_split = split[2].split('  ')
+
+head_gov = gov_split[len(gov_split) - 1]
+
+print("\t\t" + state + "\nThe current Head of State is: " + head_state + "\nThe current Head of Government is: " + head_gov)
 
 print("Response status code: " + str(response.status_code))
