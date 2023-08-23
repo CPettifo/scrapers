b5605b0ad3f49916ae4c4c440dc1bdb0a1a5e8e6
CPettifo <cpettifo@myune.edu.au>
2023-08-23 16:57:56 +0200

    output one single row from table

--- leaders/leaders.py
+++ leaders/leaders.py
@@ -1,8 +1,9 @@
 try:
     import requests
     from bs4 import BeautifulSoup
+    import pandas as pd
 except ImportError:
-    print("You need the requests and beautifulsoup4 pip modules")
+    print("You need the requests and beautifulsoup4 pip modules, oh yeah and pandas")
 
 response = requests.get(
     url="https://en.wikipedia.org/wiki/List_of_current_heads_of_state_and_government"
@@ -10,8 +11,11 @@ response = requests.get(
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
-title = soup.find('table',{'class':"wikitable"})
+table = soup.find('table',{'class':"wikitable"})
 
-print(title)
+df = pd.read_html(str(table))
+df=pd.DataFrame(df[0])
+
+print(str(df.loc[df.index[12]]))
 
 print("Response status code: " + str(response.status_code))
